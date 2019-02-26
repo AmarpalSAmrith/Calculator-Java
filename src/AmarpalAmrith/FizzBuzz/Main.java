670e91b6faa10a90e7473e236db3ef95f3eee108
Amarpal.Amrith <amarpal.amrith@AmarpalAmriths-MacBook-Pro.local>
2019-02-26 12:19:07 +0000

    Created Calc with 3 operators

--- src/AmarpalAmrith/FizzBuzz/Main.java
+++ src/AmarpalAmrith/FizzBuzz/Main.java
@@ -1,8 +1,25 @@
 package AmarpalAmrith.FizzBuzz;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+    Random Rn = new Random();
+    int randomNumber = Rn.nextInt(100);
+    int randomNumber2 = Rn.nextInt(100);
+    System.out.println(randomNumber);
+	System.out.println(randomNumber2);
+    Scanner Input = new Scanner(System.in);
+    String symbol = Input.next();
+    if (symbol.equals("*")) {
+        System.out.println(randomNumber * randomNumber2);
+    } else if (symbol.equals("-")) {
+        System.out.println(randomNumber - randomNumber2);
+    } else if (symbol.equals("+")) {
+        System.out.println(randomNumber + randomNumber2);
+    }
+
     }
 }
