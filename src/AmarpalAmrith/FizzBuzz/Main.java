49434ff60fc0df1587e7ab358413f9555e05d132
Amarpal.Amrith <amarpal.amrith@AmarpalAmriths-MacBook-Pro.local>
2019-02-26 14:00:22 +0000

    User inputs numbers

--- src/AmarpalAmrith/FizzBuzz/Main.java
+++ src/AmarpalAmrith/FizzBuzz/Main.java
@@ -1,25 +1,50 @@
 package AmarpalAmrith.FizzBuzz;
 
+import com.sun.java.util.jar.pack.Instruction;
+
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-    Random Rn = new Random();
-    int randomNumber = Rn.nextInt(100);
-    int randomNumber2 = Rn.nextInt(100);
-    System.out.println(randomNumber);
-	System.out.println(randomNumber2);
-    Scanner Input = new Scanner(System.in);
-    String symbol = Input.next();
-    if (symbol.equals("*")) {
-        System.out.println(randomNumber * randomNumber2);
-    } else if (symbol.equals("-")) {
-        System.out.println(randomNumber - randomNumber2);
-    } else if (symbol.equals("+")) {
-        System.out.println(randomNumber + randomNumber2);
-    }
+    System.out.println("Welcome to the Calculator!");
 
+    Random Rn = new Random();
+    Scanner Input = new Scanner(System.in);
+    System.out.println("Please enter the first Number:");
+    int No1 = Input.nextInt();
+    System.out.println("Please enter the second Number:");
+    int No2 = Input.nextInt();
+    System.out.println("Please enter the operator:");
+    String symbol = Input.next();
+    System.out.println("Number 1 is: " + No1);
+
+	System.out.println("Number 2 is: " + No2);
+	String operator = "";
+	switch (symbol){
+	    case "+" :
+	        operator = "Add";
+	        break;
+        case "-":
+            operator = "Subtract";
+            break;
+        case "*":
+            operator = "Multiple";
+            break;
+        case "/":
+            operator = "Divide";
+            break;
+    }
+    System.out.println("Calculation is: " + No1 + " " + operator + " " + No2);
+    if (symbol.equals("*")) {
+        System.out.println(No1 * No2);
+    } else if (symbol.equals("-")) {
+        System.out.println(No1 - No2);
+    } else if (symbol.equals("+")) {
+        System.out.println(No1 + No2);
+    } else if (symbol.equals("/")) {
+        System.out.println(No1 / No2);
+    }
     }
 }
